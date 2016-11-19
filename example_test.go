3e457b91169ff44cdfacc7215fd5df25277e7960
Tianon Gravi <admwiggin@gmail.com>
2016-11-18 23:42:36 -0800

    Add an initial ExampleTickString

---
+++ example_test.go
@@ -0,0 +1,37 @@
+package progress_test
+
+import (
+	"fmt"
+
+	"go.tianon.xyz/progress"
+)
+
+func ExampleTickString() {
+	bar := progress.NewBar(nil)
+	bar.Min = -50
+	bar.Max = 50
+
+	bar.Prefix = func(_ *progress.Bar) string {
+		return "["
+	}
+	bar.Suffix = func(b *progress.Bar) string {
+		return fmt.Sprintf("]%3.0f%%", b.Progress()*100)
+	}
+
+	bar.Phases = []string{
+		" ",
+		"-",
+		"=",
+	}
+
+	for bar.Val = bar.Min; bar.Val <= bar.Max; bar.Val += 25 {
+		fmt.Println(bar.TickString(8))
+	}
+
+	// Output:
+	// [  ]  0%
+	// [- ] 25%
+	// [= ] 50%
+	// [=-] 75%
+	// [==]100%
+}
